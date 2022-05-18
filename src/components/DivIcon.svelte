6da84615327f8c659550587d7647dc562e68be41
Nicola Giuliani <nicola.giuliani@outlook.com>
2022-05-18 22:10:05 +0200

    feat: add DivIcon type

---
+++ src/components/DivIcon.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {getContext} from 'svelte';
+    import L from 'leaflet';
+
+    const {getMarker} = getContext(L.Marker);
+
+    export let options = {};
+
+    let icon;
+    let html;
+
+    $: {
+        icon = L.divIcon({...{html}, ...options});
+        getMarker().setIcon(icon);
+    }
+
+    export function getIcon() {
+        return icon;
+    }
+</script>
+
+<div bind:this={html}>
+    <slot></slot>
+</div>
