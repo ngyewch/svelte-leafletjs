f461987756a79aff139ebd1314f07b9ef823cc4f
Nick Ng <ngyewch@gmail.com>
2024-02-29 16:35:17 +0800

    feat: updated Icon options

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
     import {getContext} from 'svelte';
-    import {Icon, type IconOptions, Marker} from 'leaflet';
+    import {Icon, type BaseIconOptions, type IconOptions, Marker} from 'leaflet';
 
     import type {MarkerProvider} from '../lib/context.js';
 
     const markerProvider = getContext<MarkerProvider>(Marker);
 
     export let iconUrl: string;
-    export let options: IconOptions = {
-        iconUrl: '',
-    };
+    export let options: BaseIconOptions = {};
 
     let icon: Icon;
 
     $: {
         if (!icon) {
-            const adjustedOptions = {...options};
-            if (iconUrl) {
-                adjustedOptions.iconUrl = iconUrl;
-            }
+            const adjustedOptions: IconOptions = {
+                ...options,
+                iconUrl: iconUrl,
+            };
             icon = new Icon(adjustedOptions);
             markerProvider().setIcon(icon);
         }
